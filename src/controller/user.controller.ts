24c3249835419490055764e742bfd1c3635fe955
Hardik7843 <hardikprajapati7843@gmail.com>
2025-07-27 16:31:31 +0530

    change

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -3,7 +3,7 @@ import { users } from "@/db/schema";
 import { Request, Response } from "express";
 
 export const makeUser = async (req: Request, res: Response): Promise<any> => {
-  console.log("hiiii");
+  console.log("hiiiiiii");
 
   return res.status(200).send({ user: {} });
 };
